0f93147e798ed262f5ab9f6dc69edb4b127a7417
Nomany <nomany@live.ru>
2024-08-11 17:11:47 +0100

    Initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { address, toNano } from "@ton/core";
+import { MainContract } from "../wrappers/MainContract";
+import { compile, NetworkProvider } from "@ton/blueprint";
+
+export async function run(provider: NetworkProvider) {
+  const myContract = MainContract.createFromConfig(
+    {
+      number: 0,
+      address: address("kQDPKoik0b-fi0pGukkc3GjzLgtTmh0118kizMWh4nVo_GpG"),
+      owner_address: address(
+        "kQDPKoik0b-fi0pGukkc3GjzLgtTmh0118kizMWh4nVo_GpG"
+      ),
+    },
+    await compile("MainContract")
+  );
+
+  const openedContract = provider.open(myContract);
+
+  openedContract.sendDeploy(provider.sender(), toNano("0.05"));
+
+  await provider.waitForDeploy(myContract.address);
+}
